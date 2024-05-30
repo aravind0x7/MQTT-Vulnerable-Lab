27e879b23570588361f7d5e0c7cd3194673ed2d8
Gnana Aravind K <97963390+aravind0x7@users.noreply.github.com>
2024-05-30 16:15:44 +0530

    Update publisher.sh

--- publisher.sh
+++ publisher.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
 
+# Loop to continuously publish messages to the MQTT broker
+while true; do
+    mosquitto_pub -t 'topic_1' -h localhost -m "Data 1" -r -u admin -P admin
+    sleep 5
+    mosquitto_pub -t 'topic_2' -h localhost -m "Data 2" -r -u admin -P admin
+    sleep 5
+    mosquitto_pub -t 'topic_3' -h localhost -m "Data 3" -r -u admin -P admin
+    sleep 3
+done
